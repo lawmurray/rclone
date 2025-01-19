6990e80a7ef4062e340f5b4daa35913227654295
Lawrence Murray <lawrence@indii.org>
2025-01-19 16:26:23 +0700

    Also remove from watcher on remove, not just rename.

--- backend/local/local.go
+++ backend/local/local.go
@@ -1818,10 +1818,7 @@ func (f *Fs) ChangeNotify(ctx context.Context, notifyFunc func(string, fs.EntryT
 						if _, ok := dirs.LoadAndDelete(event.Name); ok {
 							entryType = fs.EntryDirectory
 						}
-						if event.Has(fsnotify.Rename) {
-							// on Windows, need to remove watcher
-							watcher.Remove(event.Name)
-						}
+						watcher.Remove(event.Name)
 					} else if event.Has(fsnotify.Write) || event.Has(fsnotify.Chmod) {
 						if _, ok := dirs.Load(event.Name); ok {
 							entryType = fs.EntryDirectory
