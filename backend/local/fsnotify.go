8ea4cbdad3f5e51ea4a073cf31c9f8cf7478ee06
Lawrence Murray <lawrence@indii.org>
2025-01-20 14:21:57 +0700

    Remove conditional compile, complicating matters.

--- backend/local/fsnotify.go
+++ backend/local/fsnotify.go
@@ -3,8 +3,6 @@
 // not yet Mac, hence there is no public interface for this in fsnotify just
 // yet. This is currently only needed for, and enabled for, Windows builds.
 
-//go:build windows
-
 package local
 
 import (
