6807f4922859774dcf5ed90eac022a75ef429255
Lawrence Murray <lawrence@indii.org>
2025-01-20 14:09:52 +0700

    Add conditional compilation for recursive watch hack.

--- backend/local/fsnotify.go
+++ backend/local/fsnotify.go
@@ -1,3 +1,10 @@
+// Hack to enable recursive watchers in fsnotify, which are available for
+// Windows and Linux (although not quite what is needed for Linux here), but
+// not yet Mac, hence there is no public interface for this in fsnotify just
+// yet. This is currently only needed for, and enabled for, Windows builds.
+
+//go:build windows
+
 package local
 
 import (
@@ -6,10 +13,8 @@ import (
 	_ "github.com/fsnotify/fsnotify" // access fsnotify.enableRecurse
 )
 
-// Hack to enable recursive watchers in fsnotify, which are available for
-// Windows and Linux (although not quite what is needed for Linux here), but
-// not yet Mac, hence there is no public interface for this in fsnotify just
-// yet.
+// Setting fsnotify.enableRecurse to true enables recursive handling: paths
+// that end with with \... or /... as watched recursively.
 
 //go:linkname enableRecurse github.com/fsnotify/fsnotify.enableRecurse
 var enableRecurse bool
